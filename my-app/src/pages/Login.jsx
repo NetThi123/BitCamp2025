b5d1ea2f8785d714546312bf5f4da9d72136d012
tomatoSquirrel <menameshimuerte@gmail.com>
2025-04-12 10:31:58 -0400

    I think I might've lied the first time but idk

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -4,25 +4,33 @@ function Login() {
     return (
       <div class = "block">
         <div class = "login-block">
-            <h2 class = "login-header">LOGIN: </h2>
+            <h2 class = "login-header">LOGIN </h2>
             <form class = "login-form">
+                <div class = "form-field">
                 <p>USERNAME</p>
-                <input type="text" placeholder="Enter" />
+                <input type="text" placeholder="example@gmail.com" />
+                </div>
                 <br></br>
+                <div class = "form-field">
                 <p>PASSWORD</p>
-                <input type="password" placeholder="Enter" />
+                <input type="password" placeholder="makegoodpasswordpls123" />
+                </div>
                 <br></br>
                 <button type="submit">LOGIN</button>
             </form>
         </div>
         <div class = "login-block">
-            <h2 class = "login-header">SIGN UP:</h2>
+            <h2 class = "login-header">SIGN UP</h2>
             <form class = "login-form">
+            <div class = "form-field">
                 <p>USERNAME</p>
-                <input type="text" placeholder="Enter" />
+                <input type="text" placeholder="example@gmail.com" />
+                </div>
                 <br></br>
+                <div class = "form-field">
                 <p>PASSWORD</p>
-                <input type="password" placeholder="Enter" />
+                <input type="password" placeholder="makegoodpasswordpls123" />
+                </div>
                 <br></br>
                 <button type="submit">SIGN UP</button>
             </form>
