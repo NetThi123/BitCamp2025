3315ac6cec253fd5d2fddc2993a9882165320203
disha.ramesh <disha.ramesh@gmail.com>
2025-04-12 12:24:34 -0400

    navbar to right + silly goofy anim

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div>
+        <div className= "navbar-container">
+            <div className = "logo">
+                    <a href="/" class="logo-link">Logo</a>
+            </div>
             <nav>
                 <div className="topnav">
                     <Link to="/homepage">Home</Link>
-                    <Link to="/login">Log In/Sign Up</Link>
                     <Link to="/collegefinder">My College</Link>
+                    <Link to="/login" classNmae="login-link">Log In/Sign Up</Link>
                 </div>
             </nav>
         </div>
