e6e3b8dc660d824d1ff0f2849f0c147bd5b78c83
tomatoSquirrel <menameshimuerte@gmail.com>
2025-04-12 00:17:01 -0400

    added css

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 
 function Login() {
     return (
-      <div>
-        <h2>Login</h2>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
+      <div class = "block">
+        <div class = "login-block">
+            <h2 class = "login-header">Login</h2>
+            <form class = "login-form">
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+        <div class = "sign-up">
+        <h2 class = "sign-up-header">New? Sign Up!</h2>
+            <form class = "sign-up-form">
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
       </div>
     );
   }
