3ccde22403bf00dac31f7e2f28d64e2472fc7dd2
tomatoSquirrel <menameshimuerte@gmail.com>
2025-04-11 23:34:41 -0400

    Fixed login page problem can see now

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,18 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './pages/Login'
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <Login />
-      </div>
-    </>
-  )
+    <div>
+      <Login />
+    </div>
+  );
 }
+
 
 export default App
